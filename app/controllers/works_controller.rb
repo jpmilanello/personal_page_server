d3a2e897c4cae8c0cb60ef916da99e0eb05c7ae5
jpmilanello <jesuspiont@gmail.com>
2014-12-31 21:49:14 -0500

    server config

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,62 @@
+class WorksController < ApplicationController
+	def set_access_control_headers 
+		headers['Access-Control-Allow-Origin'] = '*' 
+		headers['Access-Control-Request-Method'] = '*' 
+	end
+	def fetch_question
+    	@work = Work.find_by_id(params[:id])
+  	end
+	def index
+	  	@works = Work.all
+	    respond_to do |format|
+	      	format.json { render json: @works }
+	      	format.xml { render xml: @works }
+	    end
+	end
+	def show
+		@work = Work.find_by_id(params[:id])
+    	respond_to do |format|
+      		format.json { render json: @work }
+      		format.xml { render xml: @work }
+    	end
+  	end
+	def create
+		@work = Work.new(work_params)
+	    respond_to do |format|
+	      	if @work.save
+	        	format.json { render json: @work, status: :created }
+	        	format.xml { render xml: @work, status: :created }
+	      	else
+	        	format.json { render json: @work.errors, status: :unprocessable_entity }
+	        	format.xml { render xml: @work.errors, status: :unprocessable_entity }
+	      	end
+	    end
+	end
+	def update
+		@work = Work.find_by_id(params[:id])
+    	respond_to do |format|
+      		if @work.update_attributes(work_params)
+        		format.json { head :no_content, status: :ok }
+        		format.xml { head :no_content, status: :ok }
+      		else
+        		format.json { render json: @work.errors, status: :unprocessable_entity }
+        		format.xml { render xml: @work.errors, status: :unprocessable_entity }
+      		end
+   		end
+  	end
+	def destroy
+		@work = Work.find_by_id(params[:id])
+	    respond_to do |format|
+	      	if @work.destroy
+	        	format.json { head :no_content, status: :ok }
+	        	format.xml { head :no_content, status: :ok }
+	      	else
+	        	format.json { render json: @work.errors, status: :unprocessable_entity }
+	        	format.xml { render xml: @work.errors, status: :unprocessable_entity }
+	      	end
+	    end
+	 end
+	def work_params
+      params.require(:work).permit(:id, :workTitle, :pathImage, :workText, :workTags)
+    end
+end
---
+++ db/migrate/20150101022338_create_works.rb
@@ -0,0 +1,12 @@
+class CreateWorks < ActiveRecord::Migration
+  def change
+    create_table :works do |t|
+      t.string :workTitle
+      t.string :pathImage
+      t.text :workText
+      t.string :workTags
+
+      t.timestamps
+    end
+  end
+end
