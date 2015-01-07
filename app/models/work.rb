5adf6916d4c26b7966e352b40f89ed58ff8dfeea
jpmilanello <jesuspiont@gmail.com>
2015-01-07 16:57:16 -0500

    decommit

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,3 @@
 class Work < ActiveRecord::Base
 	validates :workTitle,:pathImage, :workText, :workTags, :presence =>true
-	has_attached_file :pathImage, :path => ":rails_root/public/uploads/:filename"
 end
