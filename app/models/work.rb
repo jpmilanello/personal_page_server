e0a21ac010b06730f4703c46791c997f8168d1d7
jpmilanello <jesuspiont@gmail.com>
2015-01-08 00:06:57 -0500

    delete validates pathImage

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,4 @@
 class Work < ActiveRecord::Base
-	validates :workTitle,:pathImage, :workText, :workTags, :presence =>true
+	validates :workTitle, :workText, :workTags, :presence =>true
 	has_attached_file :pathImage
 end
